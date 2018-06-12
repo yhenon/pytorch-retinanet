79270386915ac637ca30a6e12eede5414bbc264c
yhenon <yannhenon@gmail.com>
2018-06-12 15:50:24 -0400

    no backprop during testing

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ dataloader_train = DataLoader(dataset_train, num_workers=4, collate_fn=collater,
 sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
 dataloader_val = DataLoader(dataset_val, num_workers=1, collate_fn=collater, batch_sampler=sampler_val)
 
-model = torch.load('csv_model_3.pt')
+model = torch.load('csv_model_1.pt')
 
 use_gpu = True
 
@@ -44,7 +44,6 @@ model.eval()
 
 unnormalize = UnNormalizer()
 
-
 def draw_caption(image, box, caption):
 
 	b = np.array(box).astype(int)
@@ -53,29 +52,31 @@ def draw_caption(image, box, caption):
 
 for idx, data in enumerate(dataloader_val):
 
-	scores, classification, transformed_anchors = model(data['img'].cuda().float())
+	with torch.no_grad():
+		st = time.time()
+		scores, classification, transformed_anchors = model(data['img'].cuda().float())
+		print('Elapsed time: {}'.format(time.time()-st))
+		idxs = np.where(scores>0.5)
+		img = np.array(255 * unnormalize(data['img'][0, :, :, :])).copy()
 
-	idxs = np.where(scores>0.5)
-	img = np.array(255 * unnormalize(data['img'][0, :, :, :])).copy()
+		img[img<0] = 0
+		img[img>255] = 255
 
-	img[img<0] = 0
-	img[img>255] = 255
+		img = np.transpose(img, (1, 2, 0))
 
-	img = np.transpose(img, (1, 2, 0))
+		img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
 
-	img = cv2.cvtColor(img.astype(np.uint8), cv2.COLOR_BGR2RGB)
+		for j in range(idxs[0].shape[0]):
+			bbox = transformed_anchors[idxs[0][j], :]
+			x1 = int(bbox[0])
+			y1 = int(bbox[1])
+			x2 = int(bbox[2])
+			y2 = int(bbox[3])
+			label_name = dataset_val.labels[int(classification[idxs[0][j]])]
+			draw_caption(img, (x1, y1, x2, y2), label_name)
 
-	for j in range(idxs[0].shape[0]):
-		bbox = transformed_anchors[idxs[0][j], :]
-		x1 = int(bbox[0])
-		y1 = int(bbox[1])
-		x2 = int(bbox[2])
-		y2 = int(bbox[3])
-		label_name = dataset_val.labels[int(classification[idxs[0][j]])]
-		draw_caption(img, (x1, y1, x2, y2), label_name)
+			cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
+			print(label_name)
 
-		cv2.rectangle(img, (x1, y1), (x2, y2), color=(0, 0, 255), thickness=2)
-		print(label_name)
-
-	cv2.imshow('img', img)
-	cv2.waitKey(0)
+		#cv2.imshow('img', img)
+		#cv2.waitKey(0)
