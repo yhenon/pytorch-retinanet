13bd8b3ed45c2e462af4e68533e6cedbcbd64475
Yann Henon <yannhenon@gmail.com>
2019-12-30 11:41:04 -0500

    add validation

---
+++ coco_validation.py
@@ -0,0 +1,44 @@
+import argparse
+import torch
+from torchvision import transforms
+
+from retinanet import model
+from retinanet.dataloader import CocoDataset, Resizer, Normalizer
+from retinanet import coco_eval
+
+assert torch.__version__.split('.')[0] == '1'
+
+print('CUDA available: {}'.format(torch.cuda.is_available()))
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(description='Simple training script for training a RetinaNet network.')
+
+    parser.add_argument('--coco_path', help='Path to COCO directory')
+    parser.add_argument('--model_path', help='Path to model', type=str)
+
+    parser = parser.parse_args(args)
+
+    dataset_val = CocoDataset(parser.coco_path, set_name='val2017',
+                              transform=transforms.Compose([Normalizer(), Resizer()]))
+
+    # Create the model
+    retinanet = model.resnet50(num_classes=dataset_val.num_classes(), pretrained=True)
+
+    use_gpu = True
+
+    if use_gpu:
+        retinanet = retinanet.cuda()
+
+    retinanet.load_state_dict(torch.load(parser.model_path))
+    retinanet = torch.nn.DataParallel(retinanet).cuda()
+
+    retinanet.training = False
+    retinanet.eval()
+    retinanet.module.freeze_bn()
+
+    coco_eval.evaluate_coco(dataset_val, retinanet)
+
+
+if __name__ == '__main__':
+    main()
