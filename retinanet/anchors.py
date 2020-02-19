8ffc5d5e1a50b43b3c330dd3ee37816920b365f5
rvandeghen <37592623+rvandeghen@users.noreply.github.com>
2020-02-19 16:58:58 +0100

    cpu compliance
    The code will work with cpu only

--- retinanet/anchors.py
+++ retinanet/anchors.py
@@ -34,7 +34,10 @@ class Anchors(nn.Module):
 
         all_anchors = np.expand_dims(all_anchors, axis=0)
 
-        return torch.from_numpy(all_anchors.astype(np.float32)).cuda()
+        if torch.cuda.is_available():
+            return torch.from_numpy(all_anchors.astype(np.float32)).cuda()
+        else:
+            return torch.from_numpy(all_anchors.astype(np.float32))
 
 def generate_anchors(base_size=16, ratios=None, scales=None):
     """
@@ -124,4 +127,3 @@ def shift(shape, stride, anchors):
     all_anchors = all_anchors.reshape((K * A, 4))
 
     return all_anchors
-
