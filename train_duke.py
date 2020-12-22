54f451fdb7780f2f19ed020a68efca160c694040
chledowski <jakub.chledowski@gmail.com>
2020-12-22 14:23:14 +0100

    Update training pipeline

--- train_duke.py
+++ train_duke.py
@@ -40,33 +40,33 @@ def boolean_string(s):
 def get_args():
     parser = argparse.ArgumentParser('Simple training script for training a RetinaNet network.')
     parser.add_argument('-p', '--project', type=str, default='duke_data_v2_loader_v1.1', help='project file that contains parameters')
-    # parser.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
-    # parser.add_argument('-n', '--num_workers', type=int, default=12, help='num_workers of dataloader')
-    # parser.add_argument('--batch_size', type=int, default=12, help='The number of images per batch among all devices')
-    # parser.add_argument('--head_only', type=boolean_string, default=False,
-    #                     help='whether finetunes only the regressor and the classifier, '
-    #                          'useful in early stage convergence or small/easy dataset')
-    # parser.add_argument('--lr', type=float, default=1e-4)
-    # parser.add_argument('--optim', type=str, default='adamw', help='select optimizer for training, '
-    #                                                                'suggest using \'admaw\' until the'
-    #                                                                ' very final stage then switch to \'sgd\'')
-    # parser.add_argument('--num_epochs', type=int, default=500)
-    # parser.add_argument('--val_interval', type=int, default=1, help='Number of epoches between valing phases')
-    # parser.add_argument('--save_interval', type=int, default=500, help='Number of steps between saving')
-    # parser.add_argument('--es_min_delta', type=float, default=0.0,
-    #                     help='Early stopping\'s parameter: minimum change loss to qualify as an improvement')
-    # parser.add_argument('--es_patience', type=int, default=0,
-    #                     help='Early stopping\'s parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.')
-    # parser.add_argument('--log_path', type=str, default='logs/')
-    # parser.add_argument('-w', '--load_weights', type=str, default=None,
-    #                     help='whether to load weights from a checkpoint, set None to initialize, set \'last\' to load last checkpoint')
-    # parser.add_argument('--saved_path', type=str, default='logs/')
-    # parser.add_argument('--debug', type=boolean_string, default=False,
-    #                     help='whether visualize the predicted boxes of training, '
-    #                          'the output images will be in test/')
-    # parser.add_argument('--lms', type=boolean_string, default=False,
-    #                     help='use large model support on skynet')
-    # parser.add_argument('--neptune_exp_name', type=str, default='duke-training', help='Name of the experiment in Neptune.')
+    parser.add_argument('-c', '--compound_coef', type=int, default=0, help='coefficients of efficientdet')
+    parser.add_argument('-n', '--num_workers', type=int, default=12, help='num_workers of dataloader')
+    parser.add_argument('--batch_size', type=int, default=12, help='The number of images per batch among all devices')
+    parser.add_argument('--head_only', type=boolean_string, default=False,
+                        help='whether finetunes only the regressor and the classifier, '
+                             'useful in early stage convergence or small/easy dataset')
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--optim', type=str, default='adamw', help='select optimizer for training, '
+                                                                   'suggest using \'admaw\' until the'
+                                                                   ' very final stage then switch to \'sgd\'')
+    parser.add_argument('--num_epochs', type=int, default=500)
+    parser.add_argument('--val_interval', type=int, default=1, help='Number of epoches between valing phases')
+    parser.add_argument('--save_interval', type=int, default=500, help='Number of steps between saving')
+    parser.add_argument('--es_min_delta', type=float, default=0.0,
+                        help='Early stopping\'s parameter: minimum change loss to qualify as an improvement')
+    parser.add_argument('--es_patience', type=int, default=0,
+                        help='Early stopping\'s parameter: number of epochs with no improvement after which training will be stopped. Set to 0 to disable this technique.')
+    parser.add_argument('--log_path', type=str, default='logs/')
+    parser.add_argument('-w', '--load_weights', type=str, default=None,
+                        help='whether to load weights from a checkpoint, set None to initialize, set \'last\' to load last checkpoint')
+    parser.add_argument('--saved_path', type=str, default='logs/')
+    parser.add_argument('--debug', type=boolean_string, default=False,
+                        help='whether visualize the predicted boxes of training, '
+                             'the output images will be in test/')
+    parser.add_argument('--lms', type=boolean_string, default=False,
+                        help='use large model support on skynet')
+    parser.add_argument('--neptune_exp_name', type=str, default='duke-training', help='Name of the experiment in Neptune.')
     parser.add_argument('--dataset', help='Dataset type, must be one of csv or coco or duke.', default='duke')
     parser.add_argument('--coco_path', help='Path to COCO directory')
     parser.add_argument('--csv_train', help='Path to file containing training annotations (see readme)')
